3ec8cb6805c0c8f0b74cf54126ab296cb10f3261
Will Miller <millerw4@miamioh.edu>
2023-09-20 12:42:13 -0400

    commented out iframe fullscreen experimental feature

--- components/Card.js
+++ components/Card.js
@@ -1,3 +1,4 @@
+// import { useState } from 'react'
 const embedStyle = {
   height: '100%',
   width: '100%',
@@ -7,18 +8,28 @@ const embedStyle = {
 //   overflow: 'hidden'
 // };
 const Card = ({ embedLink, handleVote }) => {
+  // const [fullscreen, setFullscreen] = useState(false)
+  // const clickHandler = (e) => {
+  //   console.log('handling click on iframe')
+  //   if (fullscreen) {
+  //     document.exitFullscreen()
+  //   } else {
+  //     Element.requestFullscreen();
+  //   }
+  //   setFullscreen(f => !f)
+  // }
   return (
     // <div className='wrap'>
     //   <iframe className='scaled-frame' loading='lazy' sandbox='' scrolling='yes' src={embedLink}></iframe>
     // </div>
-    <div className="card">
+    <div className="card" >
       <div className="main-content">
         <div className="btn-container">
           <button className="btn delete" onClick={handleVote}>
             Vote
           </button>
         </div>
-        <iframe loading='lazy' sandbox='' scrolling='yes' style={embedStyle} src={embedLink}></iframe>
+        <iframe loading='lazy' sandbox='' scrolling='yes' allowFullScreen='true' style={embedStyle} src={embedLink}></iframe>
       </div>
     </div>
   )
